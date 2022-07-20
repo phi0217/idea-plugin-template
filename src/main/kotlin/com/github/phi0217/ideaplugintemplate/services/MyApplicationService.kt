8411ac62b79fdd9a00748e73a7cee2d474e8f621
GitHub Action <action@github.com>
2022-07-20 09:01:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/phi0217/ideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.phi0217.ideaplugintemplate.services
+
+import com.github.phi0217.ideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
